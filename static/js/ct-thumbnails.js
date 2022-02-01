73b1a047218b5356e218a73fdf98062ce6076e55
Jan Drewnak <j.drewnak@conterra.de>
2022-02-01 21:10:45 +0100

    Adjust text size and add modal image styles

---
+++ static/js/ct-thumbnails.js
@@ -0,0 +1,21 @@
+function openModal(image) {
+    var imageId = image.id;
+    var modal = document.getElementById(imageId + "-modal");
+    var img = document.getElementById(imageId);
+    var modalImg = document.getElementById(imageId + "-modal-img");
+    var captionText = document.getElementById(imageId + "-modal-caption");
+    modal.style.display = "block";
+    modalImg.src = img.src;
+    captionText.innerHTML = img.alt;
+
+    var span = document.getElementById(imageId + "-modal-close");
+
+    span.onclick = function() { 
+        modal.style.display = "none";
+    }
+    document.onkeydown = function(evt) { 
+        if (evt && (evt.key === "Esc" || evt.key === "Escape")) {
+            modal.style.display = "none";
+        }
+    }
+}
